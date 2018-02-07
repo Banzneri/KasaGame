5295f148608fbcc88db9c790dc6fb544e0543a9f
Banzneri <pasi.hanhijarvi@gmail.com>
2018-02-07 23:45:40 +0200

    Added moving platforms and a new demo scene, Demo1

---
+++ KasaGame/Assets/Scripts/Hover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+	enum SinCos {
+		Sin,
+		Cos
+	};
+	[SerializeField] private float min = -1f;
+	[SerializeField] private float max = 1f;
+	[SerializeField] private float speed = 0.5f;
+	[SerializeField] private SinCos sinCos;
+	private float randomDelay;
+	private float delayCounter = 0;
+	private Vector3 lowPoint;
+	private Vector3 highPoint;
+	private float curSpeed = 1;
+	private Vector3 originalPos;
+	// Use this for initialization
+	void Start () {
+		randomDelay = Random.Range(0.5f, 2f);
+		originalPos = transform.position;
+		highPoint = new Vector3(originalPos.x, originalPos.y + max, originalPos.z);
+		lowPoint = new Vector3(originalPos.x, originalPos.y + min, originalPos.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (delayCounter < randomDelay)
+		{
+			delayCounter += Time.deltaTime;
+		} else
+		{
+			float yPos = sinCos.Equals(SinCos.Sin) ? Mathf.Sin(Time.time) : Mathf.Cos(Time.time); 
+			this.transform.position = new Vector3(transform.position.x, transform.position.y + yPos * Time.deltaTime * speed, transform.position.z);
+		}
+	}
+}
---
+++ KasaGame/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+	[SerializeField] private Transform startLocation;
+	[SerializeField] private Transform endLocation;
+	[SerializeField] private float speed;
+	[SerializeField] private float waitTime;
+
+	private Vector3 currentDestination;
+	private float waitCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+		currentDestination = new Vector3(startLocation.position.x, startLocation.position.y, startLocation.position.z);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 trajectory = Vector3.MoveTowards(transform.position, currentDestination, speed * Time.deltaTime);
+		transform.position = trajectory;
+
+		if (transform.position == startLocation.position)
+		{
+			waitCounter += Time.deltaTime;
+			if (waitCounter > waitTime)
+			{
+				currentDestination = endLocation.position;
+				waitCounter = 0;
+			}
+		} else if (transform.position == endLocation.position)
+		{
+			waitCounter += Time.deltaTime;
+			if (waitCounter > waitTime)
+			{
+				currentDestination = startLocation.position;
+				waitCounter = 0;	
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.gameObject.transform.parent = transform;
+		}
+	}
+
+	void OnCollisionExit(Collision other)
+	{
+			if (other.gameObject.tag == "Player")
+		{
+			other.gameObject.transform.parent = null;
+		}		
+	}
+}
