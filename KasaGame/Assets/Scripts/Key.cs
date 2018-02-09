996d802c589f547e68370f128d3e687de0419e7d
Kai Heikkila <kai-kristian.heikkila@cs.tamk.fi>
2018-02-09 14:42:09 +0200

    Added key, door, box pushing

---
+++ KasaGame/Assets/Scripts/Key.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    public bool inTrigger;
+    public Door doorReference;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    private void Update()
+    {
+        if (inTrigger)
+        {
+            if(Input.GetButtonDown("action"))
+            {
+                doorReference.doorKey = true;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (inTrigger)
+        {
+            GUI.Box(new Rect(450, 400, 200, 25), "Press E to take key");
+        }   
+    }
+}
