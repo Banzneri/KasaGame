944b116a4cc11e2ffe0e06073e3e10f272198e12
Unknown <pasi.hanhijarvi@gmail.com>
2018-03-11 03:47:44 +0200

    Add preliminary data objects for saving/loading

---
+++ KasaGame/Assets/Scripts/GameManager/SceneData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneData {
+	public GameObject[] checkPoints;
+	public GameObject currentCheckpoint;
+	public int health;
+	public GameObject[] items;
+}
+
+class CheckPointData {
+	public int pos;
+	public bool isActivated;
+}
+
+class ItemData {
+	public string id;
+	public string partnerId;
+}
