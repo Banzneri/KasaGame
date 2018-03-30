68411051241277b8c0d1f3eb1053470d4da610cf
Unknown <pasi.hanhijarvi@gmail.com>
2018-03-30 14:41:09 +0300

    Add Kai's branch

---
+++ KasaGame/Assets/Scripts/Puzzles/LightsOut/LightsOut.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsOut : MonoBehaviour, ITriggerObject<IActionObject>
+{
+    private List<LightsOutCube> _cubes = new List<LightsOutCube>();
+    private bool _unlocked = false;
+    public int activeCubes = 0;
+    public GameObject[] actionObjects;
+    private AudioSource _successSound;
+
+    private void Start()
+    {
+        _successSound = GetComponent<AudioSource>();
+        foreach (Transform trans in gameObject.transform)
+        {
+            _cubes.Add(trans.gameObject.GetComponentInChildren<LightsOutCube>());
+            if (trans.gameObject.GetComponentInChildren<LightsOutCube>().activated == true)
+            {
+                activeCubes++;
+            }
+        }
+        Debug.Log(_cubes.Count);
+    }
+
+    private void Update()
+    {
+
+        if (activeCubes == _cubes.Count && _unlocked == false)
+        {
+            _unlocked = true;
+            TriggerAll();
+            _successSound.Play();
+        }
+        else if(activeCubes != _cubes.Count && _unlocked == true)
+        {
+            _unlocked = false;
+            TriggerAll();
+        }
+    }
+
+    public void Trigger(IActionObject actionObject)
+    {
+        actionObject.Action();
+    }
+
+    public void TriggerAll()
+    {
+        for (int i = 0; i < actionObjects.Length; i++)
+        {
+            actionObjects[i].GetComponent<IActionObject>().Action();
+        }
+    }
+}
---
+++ KasaGame/Assets/Scripts/Puzzles/LightsOut/LightsOutCube.cs
@@ -0,0 +1,94 @@
+﻿using Invector.CharacterController;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsOutCube : MonoBehaviour {
+    public LightsOutCube[] adjacents;
+    public bool activated;
+    private Light _light;
+    private MeshRenderer _renderer;
+    private GameObject _player;
+    private bool _playerOnTop;
+    private bool _playerJustEntered = false;
+    public AudioSource _soundOn;
+    public AudioSource _soundOff;
+    private LightsOut _manager;
+
+    private void Start()
+    {
+        _manager = gameObject.GetComponentInParent<LightsOut>();
+        _light = GetComponent<Light>();
+        _renderer = GetComponent<MeshRenderer>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+        if (activated)
+        {
+            _light.enabled = true;
+            _renderer.material.color = Color.green;
+        }
+        else
+        {
+            _light.enabled = false;
+            _renderer.material.color = Color.grey;
+        }
+    }
+
+
+    public void Change()
+    {
+        if (activated)
+        {
+            if (!_soundOff.isPlaying)
+            {
+                _soundOff.Play();
+            }
+            activated = false;
+            _light.enabled = false;
+            _renderer.material.color = Color.grey;
+            _manager.activeCubes--;
+        }
+        else
+        {
+            if (!_soundOn.isPlaying)
+            {
+                _soundOn.Play();
+            }
+            activated = true;
+            _light.enabled = true;
+            _renderer.material.color = Color.green;
+            _manager.activeCubes++;
+        }
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Ray downward = new Ray(_player.transform.position, _player.transform.TransformDirection(new Vector3(0, -0.5f, 0)));
+
+        Debug.DrawRay(_player.transform.position, _player.transform.TransformDirection(new Vector3(0, -0.5f, 0)), Color.red);
+
+        if (Physics.Raycast(downward, out hit, 1))
+        {
+            if (hit.collider == transform.GetComponent<BoxCollider>())
+            {
+                _playerOnTop = true;
+            }
+        }
+        else
+        {
+            _playerOnTop = false;
+            _playerJustEntered = false;
+        }
+
+        if (_playerOnTop == true && _playerJustEntered == false)
+        {
+            _playerJustEntered = true;
+            Change();
+            for (int i = 0; i < adjacents.Length; i++)
+            {
+                adjacents[i].Change();
+            }
+        }
+    }
+}
