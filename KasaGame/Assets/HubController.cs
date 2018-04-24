6370f7437e1a48e14ec7914b63ea367bce811e8a
HALPCat <halpcat@hotmail.com>
2018-04-25 01:14:24 +0300

    Made a HubController script, tweaked SceneChange script to work better in conjunction with it. HubController can enable and disable teleporters in Level_Hub 1.

---
+++ KasaGame/Assets/HubController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] levelTeleporters;
+    
+	void Start () {
+        ToggleTeleporter(levelTeleporters[3], false);
+        ToggleTeleporter(levelTeleporters[4], false);
+        ToggleTeleporter(levelTeleporters[5], false);
+
+        ToggleTeleporter(6, false);
+        ToggleTeleporter(7, false);
+        ToggleTeleporter(8, false);
+    }
+	
+	void Update () {
+		
+	}
+
+    SceneChange FindSceneChange(GameObject teleporterPrefab)
+    {
+        SceneChange sceneChange = teleporterPrefab.GetComponentInChildren<SceneChange>();
+        return sceneChange;
+    }
+
+    SceneChange FindSceneChange(int teleporterIndex)
+    {
+        if (teleporterIndex > levelTeleporters.Length-1) {
+            Debug.LogError("Attempted to toggle teleporter index " + teleporterIndex + " when there are " + (levelTeleporters.Length - 1) + " indexes");
+            return null;
+        }else {
+            SceneChange sceneChange = levelTeleporters[teleporterIndex].GetComponentInChildren<SceneChange>();
+            return sceneChange;
+        }
+    }
+
+    public void ToggleTeleporter(GameObject teleporter, bool onOff)
+    {
+        FindSceneChange(teleporter).ToggleTeleporter(onOff);
+    }
+
+    public void ToggleTeleporter(int teleporterIndex, bool onOff)
+    {
+        if(teleporterIndex > levelTeleporters.Length - 1 ) {
+            Debug.LogError("Attempted to toggle teleporter index " + teleporterIndex + " when there are " + (levelTeleporters.Length - 1) + " indexes");
+            return;
+        }
+        FindSceneChange(teleporterIndex).ToggleTeleporter(onOff);
+    }
+}
--- KasaGame/Assets/Scripts/Behaviour/SceneChange.cs
+++ KasaGame/Assets/Scripts/Behaviour/SceneChange.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class SceneChange : MonoBehaviour {
 	[SerializeField] private string level;
 
+    [SerializeField]
+    bool teleporterEnabled = true;
+
 	void OnTriggerEnter(Collider other)
 	{
-		if (other.tag == "Player")
+		if (other.tag == "Player" && teleporterEnabled)
 		{
 			MySceneManager.LoadLevel(level);
 		}
 	}
+
+    public void ToggleTeleporter(bool onOff)
+    {
+        teleporterEnabled = onOff;
+        if (teleporterEnabled) {
+            GetComponent<MeshRenderer>().enabled = true;
+        }else {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
 }
